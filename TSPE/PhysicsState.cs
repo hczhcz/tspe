501296d79e977103ca7d2feaf1ed38c44409568f
hcz <h.cz@qq.com>
2018-04-18 00:03:24 -0400

    implement simulation

--- TSPE/PhysicsState.cs
+++ TSPE/PhysicsState.cs
@@ -11,15 +11,39 @@
         public Vector AngularVelocity;
         public Quaternion Rotation;
 
-        public PhysicsState(Rigidbody rigidbody)
+        public PhysicsState(
+            Rigidbody rigidbody,
+            Vector velocity,
+            Vector position,
+            Vector angularVelocity,
+            Quaternion rotation
+        )
         {
             Rigidbody = rigidbody;
 
-            Velocity = new Vector();
-            Position = new Vector();
-            AngularVelocity = new Vector();
-            Rotation = new Quaternion();
+            Velocity = velocity;
+            Position = position;
+            AngularVelocity = angularVelocity;
+            Rotation = rotation;
         }
 
+        public void Simulate(PhysicsInput physicsInput, double time)
+        {
+            Position += Velocity * (0.5 * time);
+            Velocity += physicsInput.Velocity
+                + physicsInput.Acceleration * time;
+            Position += Velocity * (0.5 * time);
+
+            Rotation = Rotation.Transform(new Quaternion(
+                0,
+                AngularVelocity * (0.25 * time)
+            ));
+            AngularVelocity += physicsInput.AngularVelocity
+                + physicsInput.AngularAcceleration * time;
+            Rotation = Rotation.Transform(new Quaternion(
+                0,
+                AngularVelocity * (0.25 * time)
+            ));
+        }
     }
 }
